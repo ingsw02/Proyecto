8a21d1159f33f5189d21952223881505f313ba92
Jonathan <eseeme@root>
2016-09-19 13:21:57 -0300

    Actualizacion tablas admins/clientes password -> password_digest

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'plainpage#index'
-
+   #match ':controller(/:action(/:id))', :via => [:get, :post]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
